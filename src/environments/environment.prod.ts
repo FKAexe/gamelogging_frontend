91f1dad6cbbdc682a0af11ddadbb9ded0faf8b34
fkaexe <josemiguelmartinezay@gmail.com>
2026-03-31 19:17:23 +0200

    Enviroment API Url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://YOUR_RAILWAY_API_URL', // Replace with your Railway service URL after deployment
+  apiUrl: 'gamelogging-api-production.up.railway.app', // Replace with your Railway service URL after deployment
 };
