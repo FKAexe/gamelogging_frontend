e77543464ca83dbd1754ed25b820845c1b57621d
fkaexe <josemiguelmartinezay@gmail.com>
2026-03-31 19:52:55 +0200

    FIX Api URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'gamelogging-api-production.up.railway.app', // Replace with your Railway service URL after deployment
+  apiUrl: 'https://gamelogging-api-production.up.railway.app', // Replace with your Railway service URL after deployment
 };
